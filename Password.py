2017983e88d1dbf500f6bdf15069d653bbe716fe
adarshmoolya <75081451+adarshmoolya@users.noreply.github.com>
2021-05-23 15:53:04 +0530

    Password

--- Password.py
+++ Password.py
@@ -6,11 +6,17 @@ def count_chars(str):
           elif str[i] >= '0' and str[i] <= '9': number_ctr += 1
           else: special_ctr += 1
      return upper_ctr, lower_ctr, number_ctr, special_ctr
-           
-str = int(input())
-print("Strings:",str)
-u, l, n, s = count_chars(str)
-print('\nUpper case characters: ',u)
-print('Lower case characters: ',l)
-print('Number case: ',n)
-print('Special case characters: ',s)
+
+num = int(input("Enter length of String:"))
+
+password  = input("Enter Password:")
+u, l, n, s = count_chars(password)
+print('Special characters: ',s)
+print('Digits: ',n)
+print('\nUppercase alphabets: ',u)
+print('Lowercase alphabets: ',l)
+
+if(num<8):
+    print((8 - num)," more characters should be added" )
+else:
+    print("Strong Password. Good to go!")
